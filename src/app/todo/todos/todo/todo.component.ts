1313131d6c4e02f8934280de70ee0a84760abab8
keunkyeongKim <woodeekim@gmail.com>
2020-04-07 23:49:35 +0900

    Update css for todo-app

--- src/app/todo/todos/todo/todo.component.ts
+++ src/app/todo/todos/todo/todo.component.ts
@@ -4,15 +4,55 @@ import {Todo} from './share/todo.model';
 @Component({
   selector: 'app-todo',
   template: `
-    <input type="checkbox"  [checked]="todo.done">{{todo.text}}
+    <input type="checkbox"  [checked]="todo.done"><label>{{todo.text}}</label>
+
   `,
   styles: [`
-  host {
+  :host {
     display: block;
     padding: 16px;
     background-color: white;
     color: darkgray;
    }
+
+    input {
+      position: relative;
+    }
+
+    input:before {
+      content: "";
+      display: inline-block;
+      width: 20px;
+      height: 20px;
+      background-color: white;
+      border-radius: 20px;
+      position: absolute;
+      top: -6px;
+      left: -8px;
+      border: 1px solid darkgray;
+    }
+
+    input:checked:after {
+      content: '\\2713';
+      display: inline-block;
+      font-size: 18px;
+      width: 20px;
+      height: 20px;
+      border-radius: 20px;
+      position: absolute;
+      top: -6px;
+      left: -8px;
+      border: 1px solid dimgray;
+      background-color: dimgray;
+      text-align: center;
+      color: white;
+    }
+
+    imput:checked + label {
+      text-decoration: line-through;
+    }
+
+
 
   `]
 })
